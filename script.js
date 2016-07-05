a4dc97f1734a457daa38ab809760b1fadb7889ae
Gabriel Côté-Carrier <gabriel.cote-carrier@polymtl.ca>
2016-07-05 14:12:57 -0400

    Addition of a cell-only dropdown menu. Obviously needs graphical work. Should consider adding a button to make it appear-disappear. Also began work on the html writing guide

--- script.js
+++ script.js
@@ -1,10 +1,21 @@
 
 $(document).ready(function(){
 	$(".content").hide();
+	$(".dropdown").hide();
 	$("#content_home").show();
 })
 
 function showContent(content){
 	$(".content").hide();
 	$("#"+content).show();
+}
+function showDropdown(buttons){
+	$("#"+buttons).show("fast");
+}
+function hideDropdown(){
+	$(".dropdown").hide("fast");
+}
+function toggleDropdown(buttons){
+
+	$("#"+buttons).toggle("fast");
 }
